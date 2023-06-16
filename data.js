3341f2dcbb89a156a2fe38201a8bc3c7eae501f8
GOKUL_GOKU <115863581+GOKULMASIVE@users.noreply.github.com>
2023-06-16 21:07:51 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+const articles=[
+    {
+        id:1,
+        title:'the WET Cpdbase',
+        date:new Date(2020,9,4),
+        length:11,
+        snippet:`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas esse error nam. Iusto, assumenda voluptate.`
+},
+{
+        id:2,
+        title:'goodbye, clean code',
+        date: new Date(2019,10,22),
+        length:5,
+        snippet:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ullam numquam! Adipisci, numquam saepe. Totam.`
+},
+{
+    id:3,
+    title:'my decade in review',
+    date:new Date(2018,7,11),
+    length:5,
+    snippet:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium tempora esse labore, commodi magni ex?`
+},
+{
+    id:4,
+    title:'Hello frnds',
+    date:new Date(2017,5,11),
+    length:8,
+    snippet:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde libero dolores, adipisci quia iste est.`,
+}
+]
